0318cbf0fa829ab8ed3c38bda2b7fe070c397685
kimn <a9523023650@gmail.com>
2022-08-21 15:19:05 +0800

    [新增] Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import { useState } from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
 
-function App() {
-    const [count, setCount] = useState(0);
-
+export default function App() {
     return (
-        <div className="App">
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+        <div>
+            <header>
+                <h1>Welcome to React Router!</h1>
+            </header>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about" element={<About />} />
+                <Route path="*" element={<h2>無此路徑</h2>} />
+            </Routes>
         </div>
     );
 }
-
-export default App;
